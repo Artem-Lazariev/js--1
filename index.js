dfdf680382fda8ecba1735db7626c4faf93d8a26
artem <Artem-lazariev>
2026-05-13 17:56:40 +0200

    test

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('Happy developing ✨')
+const radios = document.querySelectorAll('input[name="color"]');
+const colorOutput = document.getElementById('output');
+
+radios.forEach(radio => {
+    radio.addEventListener('change', event => {
+        colorOutput.textContent = `Selected color: ${event.target.value}`;
+    });
+});
+
+const nameInput = document.getElementById('name-input');
+const nameOutput = document.getElementById('name-output');
+
+nameInput.addEventListener('input', () => {
+    nameOutput.textContent = nameInput.value.trim()
+        ? nameInput.value
+        : 'незнайомець';
+});
+
+const validationInput = document.getElementById('validation-input');
+
+validationInput.addEventListener('blur', () => {
+    const requiredLength = Number(validationInput.dataset.length);
+
+    if (validationInput.value.length === requiredLength) {
+        validationInput.style.border = '3px solid #4caf50';
+    } else {
+        validationInput.style.border = '3px solid #f44336';
+    }
+});
